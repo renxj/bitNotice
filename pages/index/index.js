e41af01d978320b0b73e8751cf2c4ada114fe31a
任小娟 <renxiaojuan@suiyueyule.com>
2018-02-08 20:01:29 +0800

    左滑删除

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,9 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    listData: ['BTC', 'BCH','ETH','ETC'],
+    delBtnWidth: 180//删除按钮宽度单位（rpx）
   },
   //事件处理函数
   bindViewTap: function() {
@@ -16,6 +18,8 @@ Page({
     })
   },
   onLoad: function () {
+    this.initEleWidth();
+    this.tempData();
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -44,7 +48,6 @@ Page({
     }
   },
   getUserInfo: function(e) {
-    console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
@@ -58,5 +61,128 @@ Page({
       imageUrl: 'https://static.suiyueyule.com/headerimg.png',
       path: 'pages/index/index'
     }
+  },
+  touchS: function (e) {
+    if (e.touches.length == 1) {
+      this.setData({
+        //设置触摸起始点水平方向位置
+        startX: e.touches[0].clientX
+      });
+    }
+  },
+  touchM: function (e) {
+    if (e.touches.length == 1) {
+      //手指移动时水平方向位置
+      var moveX = e.touches[0].clientX;
+      //手指起始点位置与移动期间的差值
+      var disX = this.data.startX - moveX;
+      var delBtnWidth = this.data.delBtnWidth;
+      var txtStyle = "";
+      if (disX == 0 || disX < 0) {//如果移动距离小于等于0，文本层位置不变
+        txtStyle = "left:0px";
+      } else if(disX > 0){//移动距离大于0，文本层left值等于手指移动距离
+        txtStyle = "left:-" + disX + "px";
+        if (disX >= delBtnWidth) {
+          //控制手指移动距离最大值为删除按钮的宽度
+          txtStyle = "left:-" + delBtnWidth + "px";
+        }
+      }
+      //获取手指触摸的是哪一项
+      var index = e.target.dataset.index;
+      var list = this.data.list;
+      console.log(list[index].txtStyle)
+      list[index].txtStyle = txtStyle;
+      //更新列表的状态
+      this.setData({
+        list: list
+      });
+    }
+  },
+  touchE: function (e) {
+    if (e.changedTouches.length == 1) {
+      //手指移动结束后水平位置
+      var endX = e.changedTouches[0].clientX;
+      //触摸开始与结束，手指移动的距离
+      var disX = this.data.startX - endX;
+      var delBtnWidth = this.data.delBtnWidth;
+      //如果距离小于删除按钮的1/2，不显示删除按钮
+      var txtStyle = disX > delBtnWidth / 2 ? "left:-" + delBtnWidth + "px" : "left:0px";
+      //获取手指触摸的是哪一项
+      var index = e.target.dataset.index;
+      var list = this.data.list;
+      list[index].txtStyle = txtStyle;
+      //更新列表的状态
+      this.setData({
+        list: list
+      });
+    }
+  },
+  //获取元素自适应后的实际宽度
+  getEleWidth: function (w) {
+    var real = 0;
+    try {
+      var res = wx.getSystemInfoSync().windowWidth;
+      var scale = (750 / 2) / (w / 2);//以宽度750px设计稿做宽度的自适应
+      // console.log(scale);
+      real = Math.floor(res / scale);
+      return real;
+    } catch (e) {
+      return false;
+      // Do something when catch error
+    }
+  },
+  initEleWidth: function () {
+    var delBtnWidth = this.getEleWidth(this.data.delBtnWidth);
+    this.setData({
+      delBtnWidth: delBtnWidth
+    });
+  },
+  //点击删除按钮事件
+  delItem: function (e) {
+    //获取列表中要删除项的下标
+    var index = e.target.dataset.index;
+    var list = this.data.list;
+    //移除列表中下标为index的项
+    list.splice(index, 1);
+    //更新列表的状态
+    this.setData({
+      list: list
+    });
+  },
+  //测试临时数据
+  tempData: function () {
+
+    var list = [
+      {
+        txtStyle: "",
+        icon: "https://static.suiyueyule.com/specialtopic/colour.png",
+        txt: "BTC"
+      },
+      {
+        txtStyle: "",
+        icon: "https://static.suiyueyule.com/specialtopic/colour.png",
+        txt: "BCH"
+      },
+      {
+        txtStyle: "",
+        icon: "https://static.suiyueyule.com/specialtopic/colour.png",
+        txt: "ETH"
+      },
+      {
+        txtStyle: "",
+        icon: "https://static.suiyueyule.com/specialtopic/colour.png",
+        txt: "ETC"
+      },
+      {
+        txtStyle: "",
+        icon: "https://static.suiyueyule.com/specialtopic/colour.png",
+        txt: "RMB"
+      },
+    ];
+    this.setData({
+      list: list
+    });
+
   }
+
 })
